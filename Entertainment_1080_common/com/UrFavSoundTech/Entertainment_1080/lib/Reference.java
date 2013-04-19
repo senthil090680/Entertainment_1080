4490dbca7c5e067d3430921228c55e96cb2b0111
Jonathan Montoya <Jmontoya@mail.com>
2013-04-19 14:57:32 -0700

    .
    First Commit

---
+++ Entertainment_1080_common/com/UrFavSoundTech/Entertainment_1080/lib/Reference.java
@@ -0,0 +1,28 @@
+package com.pahimar.letsmod.lib;
+
+/**
+ * LetsMod
+ * 
+ * Reference
+ * 
+ * @author pahimar
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * 
+ */
+public class Reference {
+
+    /**
+     * The mod id
+     */
+    public static final String MOD_ID = "LetsMod";
+    
+    /**
+     * The mod name
+     */
+    public static final String MOD_NAME = "Let's Mod";
+    
+    /**
+     * The version of the mod
+     */
+    public static final String VERSION = "0.0.1";
+}
